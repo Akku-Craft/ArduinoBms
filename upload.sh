3f6b614c23b8eddd7815ff239780f31513134c2c
AkkuCraft <akkucraft63@gmail.com>
2026-02-10 17:08:19 +0100

    Commit

--- upload.sh
+++ upload.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# 1. Alle Änderungen sammeln
+# 1. Zuerst deine Änderungen sammeln (Vorbereiten)
 git add .
 
-# 2. Den Commit ausführen (Fragt dich nach einer Nachricht)
+# 2. Den Commit ausführen (Lokal speichern)
 echo "Enter commit message:"
 read message
-git commit -m "$message"
+git commit -m "$message" || { echo "Nichts zu committen oder Fehler!"; }
 
-# 3. Zum ersten Repo pushen (origin)
+# 3. JETZT den Stand von GitHub holen und deine Arbeit oben drauf setzen
+echo "Checking for updates from GitHub..."
+git pull origin master --rebase || { echo "Fehler beim Pull! Konflikte müssen manuell gelöst werden."; exit 1; }
+
+# 4. Zum ersten Repo pushen (origin)
 echo "Pushing to origin..."
 git push origin master
 
-# 4. Zum zweiten Repo pushen (second-remote)
+# 5. Zum zweiten Repo pushen (second-remote)
 echo "Pushing to second-remote..."
 git push second-remote master
 
-echo "Done! Everything is synced."
+echo "Done! Everything is synced."d."
